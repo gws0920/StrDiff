c080d464f396ff0f8d88db4aaa6b11a8c3ae3e15
顾文帅 <guwenshuai@xiaomi.com>
2023-08-09 14:15:26 +0800

    feat: 显示差异总数

--- src/pages/Array/index.vue
+++ src/pages/Array/index.vue
@@ -3,10 +3,17 @@ import DiffInput from '../../components/DiffInput.vue'
 
 const valL = ref<any>()
 const valR = ref<any>()
+const diffCount = ref(0)
 
 const submit = (l: any[], r: any[]) => {
   valL.value = l
   valR.value = r
+
+  let count = 0
+  l.forEach((item, i) => {
+    if (!isSame(item, r?.[i])) count++
+  })
+  diffCount.value = count
 }
 
 const isSame = (valL: any, valR: any) => {
@@ -19,11 +26,12 @@ const isSame = (valL: any, valR: any) => {
 
 <template>
   <DiffInput :is-array="true" @submit="submit"/>
+  <code v-if="valL && valR" class="mt-2 w-full flex items-center">存在{{ diffCount }}处差异</code>
   <div class="arr border rounded mt-2 grid p-2 gap-2 grid-cols-1 overflow-auto" v-if="valL && valR">
     <div v-for="(item, index) in valL" :key="index" class="border flex w-full py-4 relative items-center justify-center flex-col">
       <code class="bg-gray-100 top-0 left-0 index absolute">{{ index }}</code>
-      <code class="val flex text-center w-full items-center justify-center whitespace-nowrap" :class="{'text-red-500': !isSame(item, valR[index])}">{{ item }}</code>
-      <code class="val flex text-center w-full items-center justify-center whitespace-nowrap" :class="{'text-red-500': !isSame(item, valR[index])}">{{ valR?.[index] }}</code>
+      <code class="val flex text-center w-full items-center justify-center whitespace-nowrap border-b border-dashed border-opacity-30" :class="{'text-red-500': !isSame(item, valR?.[index])}">{{ item }}</code>
+      <code class="val flex text-center w-full items-center justify-center whitespace-nowrap" :class="{'text-red-500': !isSame(item, valR?.[index])}">{{ valR?.[index] }}</code>
     </div>
   </div>
 </template>
--- src/pages/Array2D/index.vue
+++ src/pages/Array2D/index.vue
@@ -3,10 +3,17 @@ import DiffInput from '../../components/DiffInput.vue'
 
 const valL = ref<any>()
 const valR = ref<any>()
-
-const submit = (l: any[], r: any[]) => {
+const diffCount = ref(0)
+const submit = (l: any[][], r: any[][]) => {
   valL.value = l
   valR.value = r
+  let count = 0
+  l.forEach((row, i) => {
+    row.forEach((item, j) => {
+      if (!isSame(item, r?.[i]?.[j])) count++
+    })
+  })
+  diffCount.value = count
 }
 
 const isSame = (valL: any, valR: any) => {
@@ -23,11 +30,12 @@ const isSame = (valL: any, valR: any) => {
 
 <template>
   <DiffInput :is-array="false" @submit="submit"/>
+  <code v-if="valL && valR" class="mt-2 w-full flex items-center">存在{{ diffCount }}处差异</code>
   <div class="border rounded mt-2 grid p-2 gap-2 grid-cols-1 overflow-auto" v-if="valL && valR">
     <div class="grid gap-2" :style="{gridTemplateColumns: `repeat(${row.length}, 1fr)`}" v-for="(row, index) in valL" :key="index">
-      <div v-for="(item, j) in row" :key="j" class="border flex w-full py-4 px-4 relative items-center justify-center">
+      <div v-for="(item, j) in row" :key="j" class="border flex w-full py-4 relative items-center justify-center min-w-100px">
         <code class="bg-gray-100 top-0 left-0 index absolute">{{ index }}, {{ j }}</code>
-        <code class="flex text-center w-full max-w-80px val items-center justify-center whitespace-nowrap" :class="{'text-red-500': !isSame(item, valR[index][j])}">{{ item }} / {{ valR?.[index]?.[j] }}</code>
+        <code class="flex text-center w-full max-w-80px val items-center justify-center whitespace-nowrap" :class="{'text-red-500': !isSame(item, valR?.[index]?.[j])}">{{ item }} / {{ valR?.[index]?.[j] }}</code>
       </div>
     </div>
   </div>
